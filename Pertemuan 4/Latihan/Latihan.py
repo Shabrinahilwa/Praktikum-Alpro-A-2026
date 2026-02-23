82a0f56b241f7dc148b314a6aea36422c457d9eb
Shabrinahilwa <sabrinahilwa81@gmail.com>
2026-02-23 20:38:33 +0700

    Latihan 4

---
+++ Pertemuan 4/Latihan/Latihan.py
@@ -0,0 +1,30 @@
+#Soal No.1
+angka_list = [10, 20, 30]
+try:
+    idx = int(input('Masukkan index (0-2): '))
+    print(f'Nilai: {angka_list[idx]}')
+except ValueError:
+    print('Harus berupa angka bulat!')
+except IndexError:
+    print('Index di luar jangkauan!')
+finally:
+    print('Selesai.')
+
+#Output:
+#Masukkan index (0-2): 0 A -2
+#Harus berupa angka bulat!
+#Selesai.
+
+#Soal No.2
+try:
+    num1 = float(input("Masukkan Angka ke-1: "))
+    num2 = float(input("Masukkan Angka ke-2: "))
+
+    print(f"Hasil: {num1/num2}")
+except ValueError:
+    print("Harus berupa angka")
+except ZeroDivisionError:
+    print("Tidak bisa membagi dengan nol")
+
+
+
